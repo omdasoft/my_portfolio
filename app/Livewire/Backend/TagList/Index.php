fed823fc07870b4aa97a9bc9e9e6b639b4de115f
omdasoft <omdasoft@hotmail.com>
2026-01-22 01:48:09 +0400

    ddelete tag list

--- app/Livewire/Backend/TagList/Index.php
+++ app/Livewire/Backend/TagList/Index.php
@@ -20,4 +20,27 @@ class Index extends Component
 
         return view('livewire.backend.tag-list.index', ['list' => $list])->layout('layouts.admin');
     }
+
+    public function showConfirmationModal(int $id): void
+    {
+        $this->actionId = $id;
+        $this->dispatch('open-modal', 'confirmationModal');
+    }
+
+    public function deleteConfirmed(): void
+    {
+        if ($this->actionId !== 0) {
+            TagList::findOrFail($this->actionId)->delete();
+            $this->actionId = -1;
+        }
+
+        $this->message = 'Tag Deleted Successfully!';
+        $this->dispatch('action-success');
+        $this->closeModal('confirmationModal');
+    }
+
+    public function closeModal(string $modalName): void
+    {
+        $this->dispatch('close-modal', $modalName);
+    }
 }
