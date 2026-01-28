9ebc298f0e27db7c76d3917d5db2cf7b9b2ac5e5
omdasoft <omdasoft@hotmail.com>
2026-01-28 21:57:38 +0400

    add seo support

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,14 +1,33 @@
 <!doctype html>
 <html class="!scroll-smooth">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Emad Aldin Ali' }}</title>
+
+    <meta name="description" content="{{ $description ?? 'Senior software developer with industry experience in building scalable and high-performance applications.' }}">
+    <meta name="keywords" content="{{ $keywords ?? 'laravel, livewire, php, software, developer, engineer, full stack, node.js, vue, express.js, javascript, emad aldin' }}">
+    <meta name="author" content="Emad Aldin Ali">
+    <link rel="canonical" href="{{ $url ?? url()->current() }}">
+
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ $url ?? url()->current() }}">
+    <meta property="og:title" content="{{ $title ?? 'Emad Aldin Ali' }}">
+    <meta property="og:description" content="{{ $description ?? 'Senior software developer with industry experience in building scalable and high-performance applications.' }}">
+    <meta property="og:image" content="{{ $image ?? asset('images/default-share-image.jpg') }}">
+
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="{{ $url ?? url()->current() }}">
+    <meta property="twitter:title" content="{{ $title ?? 'Emad Aldin Ali' }}">
+    <meta property="twitter:description" content="{{ $description ?? 'Senior software developer with industry experience in building scalable and high-performance applications.' }}">
+    <meta property="twitter:image" content="{{ $image ?? asset('assets/images/profile.jpg') }}">
+
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;800&display=swap" rel="stylesheet">
-    <title>{{ $title ?? 'Emad Aldin Ali' }}</title>
+    
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
 </head>
 
 <body>
